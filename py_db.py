43de1ca0d6b2f86530141465798e8feff00b0f67
gvinicius <gvinicius@ufba.br>
2017-02-12 22:34:36 -0300

    Create tabls running well

--- py_db.py
+++ py_db.py
@@ -8,22 +8,22 @@ class DataBase:
     def create_schema(self):
         self.connect.execute("CREATE SCHEMA `linux_tests`;")
     def use_db(self):
-        self.connect.execute("USE `linux_tests`")
+        self.connect.execute("USE `linux_tests`;")
     def create_subsystems(self):
         self.use_db()
-        self.connect.execute('CREATE TABLE `subsystems`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `description` VARCHAR(15) NOT NULL )')
+        self.connect.execute('CREATE TABLE `subsystems`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `description` VARCHAR(15) NOT NULL ) ENGINE=InnoDB COLLATE=utf8_unicode_ci;')
     def create_versions(self):
         self.use_db()
-        self.connect.execute('CREATE TABLE `versions`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `description` VARCHAR(15) NOT NULL );')
+        self.connect.execute('CREATE TABLE `versions`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `description` VARCHAR(15) NOT NULL ) ENGINE=InnoDB COLLATE=utf8_unicode_ci;')
     def create_test_case(self):
         self.use_db()
-        self.connect.execute('CREATE TABLE `test_cases`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `subsystem_id` INTEGER(11) NOT NULL, `version_id` INTEGER(11) NOT NULL, `description` VARCHAR(15) NOT NULL, FOREIGN KEY (`subsystem_id`) REFERENCES subsystem(`id`), FOREIGN KEY (`version_id`) REFERENCES version(`id`));')
+        self.connect.execute('CREATE TABLE `test_cases`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `subsystem_id` INTEGER(11) NOT NULL, `version_id` INTEGER(11) NOT NULL, `description` VARCHAR(15) NOT NULL, CONSTRAINT `fk_1` FOREIGN KEY (`subsystem_id`) REFERENCES subsystems (`id`),  CONSTRAINT `fk_2` FOREIGN KEY (`version_id`) REFERENCES versions (`id`)) ENGINE=InnoDB COLLATE=utf8_unicode_ci;')
     def select_id_reverse(self, table_name):
         self.use_db()
-        return self.connect.execute('SELECT `id`, `description` FROM {}'.format(table_name))
+        return self.connect.execute('SELECT `id`, `description` FROM {0};'.format(table_name))
     def insert_common(self, table_name, description):
         self.use_db()
-        self.connect.execute('INSERT INTO {0} VALUES (`{1}`)'.format(table_name, description))
+        self.connect.execute('INSERT INTO {0} VALUES (`{1}`);'.format(table_name, description))
     def insert_common(self, description, subsystem_id, version_id):
         self.use_db()
-        self.connect.execute('INSERT INTO {0} VALUES (`{1}`, `{2}`,`{3}`)'.format(table_name, description, subsystem_id, version_id))
+        self.connect.execute('INSERT INTO `test_cases` VALUES (`{0}`, `{1}`,`{2}`);'.format(description, subsystem_id, version_id))
