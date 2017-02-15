acd0275bd6b83ace96743beb1553f5221c1d31a4
gvinicius <gvinicius@ufba.br>
2017-02-15 01:53:42 -0300

    Adjusting python style in worksheet to db

--- worksheetToBD.py
+++ worksheetToBD.py
@@ -2,57 +2,47 @@ import os
 import xlrd
 import py_bd
 
-
-root = "/Users/jonatasfbastos/Dropbox/DOUTORADO_UFBA/Computacao/Evolution/Test Evolution/LTP Study/LTP Versions/TestScript/"
+""" Relative path to linux tests data """
+root = "../evolution/"
 
 class Version:
-
 	def __init__(self, name):
 		self._name = name
 		self._sub_systems = []
 
 class SubSystem:
-
 	def __init__(self, name):
 		self._name = name
 		self._test_cases = []	
-	
 	def	__str__(self):
-		cabecalho = "----------------------------------"
-		testcases = ""
-
+		header = "----------------------------------"
+		test_cases = ""
 		for i in range(0, len(self._test_cases)):
-			testcases += str(self._test_cases[i]._name) + "\n"
-
-		return 	str(cabecalho + "\n" + self._name + "\n" + cabecalho + "\n" + testcases)
-
+			test_cases += str(self._test_cases[i]._name) + "\n"
+		return 	str(header + "\n" + self._name + "\n" + header + "\n" + test_cases)
 
 class TestCase:
-
 	def __init__(self, name):
 		self._name = name
-
 	def __str__(self):	
 		return str(self._name)
 
-
+""" Method the implements the conversion """
 def main():
 	for path, subdirs, files in os.walk(root):    
 	    for file in files:        
 	        if file.endswith(".xlsx"):
-	            workbook = xlrd.open_workbook(path + "/" + file)            
+	            workbook = xlrd.open_workbook(path + "/" + file)
 	            version = Version(file[0:12])            
                     # insert version
 	            for i in range(0, workbook.nsheets - 1):
-	            	sheet = workbook.sheet_by_index(i) 				
+	            	sheet = workbook.sheet_by_index(i)	
 	            	subsystem = SubSystem(sheet.name)
-
                         # insert subsystem?
 	            	version._sub_systems.append(subsystem)            	
 	            	for row in range(0, sheet.nrows):
 	            		test = TestCase(sheet.cell(row, 0).value)
 	            		subsystem._test_cases.append(test)
-
                                 # subsystems = py_db.DataBase.select_id_reverse(self, 'subsystems')?
                                 # versions = py_db.DataBase.select_id_reverse(self, 'versions')?
 	           		#print (subsystem)
